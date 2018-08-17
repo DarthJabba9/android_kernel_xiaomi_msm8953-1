8ec35e3df438de7ec61f0bee8bb00607bebaad53
Rama Bondan Prakoso <ramanarubp@gmail.com>
2018-08-17 13:03:14 +0700

    zipper: OC apply

--- Zipper/ramdisk/init.ramakun.sh
+++ Zipper/ramdisk/init.ramakun.sh
@@ -1,13 +1,2 @@
 #!/system/bin/sh
 
-# enable ZRAM
- 
-echo "1610612736 " >  /sys/block/zram0/disksize 
-
-echo 'lzo' > /sys/block/zram0/comp_algorithm
-
-echo 60 > /proc/sys/vm/swappiness
-
-mkswap /dev/block/zram0
-
-swapon /dev/block/zram0
